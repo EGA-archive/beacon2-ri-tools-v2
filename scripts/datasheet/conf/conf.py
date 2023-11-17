ec665bd4ed2b0df4c2468f4786a70bc32e97edb0
Oriol López-Doriga <orildsaga@gmail.com>
2023-11-17 18:20:12 +0100

    fixing column issues

--- scripts/datasheet/conf/conf.py
+++ scripts/datasheet/conf/conf.py
@@ -1,7 +1,7 @@
-num_registries=2504
-num_variants_registries=1004
+num_registries=4
+num_variants_registries=4
 num_cohorts_registries=1
 num_datasets_registries=1
 excel_filename='datasheets/CINECA_synthetic_cohort_EUROPE_UK1.xlsx'
 vcf_filename='files/vcf/chr22.Test.1000G.phase3.joint.norm.ann.dbnsfp.clinvar.cosmic.vcf.gz'
-output_docs_folder='output_docs/CINECA_synthetic_cohort_EUROPE_UK1/'
+output_docs_folder='output_docs/empty_model/'
