328a2548e091b5864badd61e4c310625c14e6230
Oriol López-Doriga <orildsaga@gmail.com>
2024-01-29 17:01:05 +0100

    splitting variants output

--- conf/conf.py
+++ conf/conf.py
@@ -3,9 +3,9 @@ csv_filename='./csv/examples/cohorts.csv'
 output_docs_folder='./output_docs/'
 
 #### VCF Conversion config parameters ####
-num_variants=1008
+num_variants=1000000
 reference_genome='GRCh38' # Choose one between NCBI36, GRCh37, GRCh38
-chromosome=['22'] # This variable is an array of chromosomes you want to convert from you vcf.gz file. Write the cromosomes the same exact way they are written in the vcf file, e.g. 'chr22', '22', ...
+chromosome=['1','2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22'] # This variable is an array of chromosomes you want to convert from you vcf.gz file. Write the cromosomes the same exact way they are written in the vcf file, e.g. 'chr22', '22', ...
 genomic_start_position=1
 genomic_end_position=1230237000000
 
