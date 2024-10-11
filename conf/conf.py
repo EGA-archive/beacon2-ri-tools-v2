45c420e3f563479df54f8542b55d90bf4978f5a0
costero-e <orildsaga@gmail.com>
2024-10-11 14:56:48 +0200

    added conf to convert phenotypic data as optional

--- conf/conf.py
+++ conf/conf.py
@@ -6,6 +6,7 @@ output_docs_folder='./output_docs/'
 allele_frequency=1 # introduce float number, leave 1 if you want to convert all the variants
 reference_genome='GRCh38' # Choose one between NCBI36, GRCh37, GRCh38
 datasetId='CINECA_synthetic_cohort_EUROPE_UK1'
+case_level_data=True
 
 ### MongoDB parameters ###
 database_host = 'mongo'
