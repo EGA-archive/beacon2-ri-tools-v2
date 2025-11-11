5a8b2f6c7e56efd73e5cf87660b065caf8594570
costero-e <orildsaga@gmail.com>
2025-11-11 11:40:07 +0100

    fixed allele frequency processing after OOC update

--- genomicVariations_vcf.py
+++ genomicVariations_vcf.py
@@ -339,6 +339,9 @@ def generate(dict_properties, args):
                 except Exception as e:
                     pass
                 if frequency_in_population == None:
+                    ref=v.REF
+                    chrom=v.CHROM
+                    start=v.start
                     print('variant in chr: {} with start position: {} and reference base: {} skipped because none of the populations had allele frequency greater than 0'.format(chrom, start, ref))
                     i+=1
                     pbar.update(1)
